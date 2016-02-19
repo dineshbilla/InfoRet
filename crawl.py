185e5ed165fad4dae396057bf04fd3024939775f
Dinesh Billa <dineshbilla@gmail.com>
2016-02-19 17:11:04 -0600

    Added delay and download of pages in XML

--- crawl.py
+++ crawl.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from bs4 import BeautifulSoup
+from time import sleep,time
 import urllib
 import re
 
@@ -8,9 +9,9 @@ seed_pages = ['/wiki/List_of_Nobel_laureates','/wiki/Academy_Awards']
 prefix = 'https://en.wikipedia.org'
 visited = set(seed_pages)
 
-#Check for file type in URL so crawler does not crawl images and text files
+#Ignores images, files, references within the same page and special sections
 def extension_scan(url):
-	file_ext = ['.png','.jpg','.jpeg','.gif','.tif','.txt','.pdf','.svg']
+	file_ext = ['.png','.jpg','.jpeg','.gif','.tif','.txt','.pdf','.svg',':','#']
 	for ext in file_ext:
 		if ext in url:
 			return True
@@ -20,10 +21,12 @@ def main_crawl():
 
 	wiki_regex = re.compile('/wiki/')
 	myfile = open('wiki.txt', 'w') #output file containing links
-
+	crawl_start = time()
 	index = 0
+	start_time = time()
 	#for seed in visited:
-	while len(visited) < 5000:
+	while len(visited) < 100000:
+		
 		#if len(visited) > 5000:
 			#break
 		seed = seed_pages[index]
@@ -39,15 +42,38 @@ def main_crawl():
 		for link in links:
 			if wiki_regex.match(link['href']) and extension_scan(link['href']) == False:
 				#print link['href']
-				if link['href'] in visited:
-					print link['href']
-				else:
+				#if link['href'] in visited:
+					#print link['href']
+				if link['href'] not in visited:#else:
 					myfile.write(link['href']+'\n')
 					visited.add(link['href'])
 					seed_pages.append(link['href'])
+					if len(visited) % 2000 == 0:
+						end_time = time()
+						print "Sites crawled: "+str(len(visited))+" time taken = "+str(end_time-start_time)
+						print " Sleeping for 3 seconds"
+						start_time = end_time
+						sleep(3)
 
+
+	myfile.close()
+	crawl_end = time()
+	print "Total time taken: "+str(crawl_end-crawl_start)
+
+def download():
+	myfile = open('wiki.txt', 'r') #output file containing links
+        outfile = open('dump.txt','w') # output file dump in XML
+	for line in myfile:
+		sleep(1)
+                link = line.replace('/wiki','/wiki/Special:Export')
+		link = prefix+line
+		r = urllib.urlopen(link).read()
+		outfile.write(r)
+	outfile.close()
+	myfile.close()
 
 
 main_crawl()
+#download()
 
 
