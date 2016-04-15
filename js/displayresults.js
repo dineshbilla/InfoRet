2d4a8605239c5f38e7354a956578f2d4c9c0a080
Dinesh Billa <dineshbilla@gmail.com>
2016-04-14 19:38:00 -0500

    More like this

--- js/displayresults.js
+++ js/displayresults.js
@@ -1,7 +1,14 @@
 
 var queryresults;
 
-function buildquery(inputtext, btitle, bcontent, bcategory) {
+function buildquery(inputtext, btitle, bcontent, bcategory, morelikethisid) {
+
+    if (morelikethisid) {
+        var url = "http://localhost:8983/solr/solrpedia/select?q=%7B!mlt+qf%3Dtext+mintf%3D2+mindf%3D2%7D" + morelikethisid;
+        url += "&fl=title+text+cat+id&rows=200&wt=json&json.wrf=callback&indent=true&facet=true&facet.field=cat";
+        return url;
+    }
+
     var url = "http://localhost:8983/solr/solrpedia/select?q=" + inputtext;
     if (btitle || bcategory || bcontent) {
         url += "&defType=dismax&qf=";
@@ -26,7 +33,7 @@ function buildquery(inputtext, btitle, bcontent, bcategory) {
             url += "cat^" + bcategory;
         }
     }
-    url += "&fl=title+text+cat&rows=100&wt=json&json.wrf=callback&indent=true&facet=true&facet.field=cat";
+    url += "&fl=title+text+cat+id&rows=1000&wt=json&json.wrf=callback&indent=true&facet=true&facet.field=cat";
     return url;
 }
 
@@ -35,14 +42,28 @@ function filterQuery(category) {
     var rblock = document.getElementById("searchresults");
     var docArray = queryresults.response.docs;
     var ndocs = 0;
-    $.each(docArray, function (key, value) {
+    $.each(docArray, function(key, value) {
         if (category && $.inArray(category, value.cat) == -1) return true;
         ndocs++;
         var searchresult = document.createElement('p');
+        var heading = document.createElement('p');
         var titlelink = document.createElement('a');
         titlelink.style.fontSize = "x-large";
         titlelink.textContent = value["title"];
         titlelink.href = "https:en.wikipedia.org/w/index.php?search=" + value["title"] + "&title=Special:Search";
+
+        var morelikethislink = document.createElement('a');
+        morelikethislink.textContent = "more like this";
+        morelikethislink.id = value["id"];
+        morelikethislink.className = "mlt";
+        morelikethislink.addEventListener("click", function() {
+            displayresults("", "", "", "", this.id);
+        }, true);
+
+        heading.appendChild(titlelink);
+        heading.appendChild(document.createElement("br"));
+        heading.appendChild(morelikethislink);
+
         var summary = document.createElement('p');
         // summary.style.fontWeight = "600";
         if (value["text"].length > 300) {
@@ -51,7 +72,7 @@ function filterQuery(category) {
             summary.innerHTML = value["text"];
         }
 
-        searchresult.appendChild(titlelink);
+        searchresult.appendChild(heading);
         searchresult.appendChild(summary);
 
         rblock.appendChild(searchresult);
@@ -61,12 +82,12 @@ function filterQuery(category) {
 
 }
 
-function displayresults(inputtext, btitle, bcontent, bcategory) {
+function displayresults(inputtext, btitle, bcontent, bcategory, morelikethisid) {
     document.getElementById("searchresults").innerHTML = "";
     document.getElementById("facets").innerHTML = "";
     // document.getElementById("numresults").innerHTML = "";
-    
-    var url = buildquery(inputtext, btitle, bcontent, bcategory);
+
+    var url = buildquery(inputtext, btitle, bcontent, bcategory, morelikethisid);
 
     $.ajax({
         type: 'GET',
@@ -74,21 +95,36 @@ function displayresults(inputtext, btitle, bcontent, bcategory) {
         dataType: 'JSONP',
         jsonpCallback: 'callback',
         crossDomain: true
-    }).done(function (data) {
+    }).done(function(data) {
         queryresults = data;
         document.getElementById("numresults").innerHTML = "Number of documents found: " + queryresults.response.numFound;
 
         var rblock = document.getElementById("searchresults");
         rblock.appendChild(document.createElement("br"));
-        
+
         // display the results --> title and text summary
-        $.each(queryresults.response.docs, function (key, value) {
+        $.each(queryresults.response.docs, function(key, value) {
             //console.log(value["title"]);
             var searchresult = document.createElement('p');
+
+            var heading = document.createElement('p');
             var titlelink = document.createElement('a');
             titlelink.style.fontSize = "x-large";
             titlelink.textContent = value["title"];
             titlelink.href = "https:en.wikipedia.org/w/index.php?search=" + value["title"] + "&title=Special:Search";
+
+            var morelikethislink = document.createElement('a');
+            morelikethislink.textContent = "more like this";
+            morelikethislink.id = value["id"];
+            morelikethislink.className = "mlt";
+            morelikethislink.addEventListener("click", function() {
+                displayresults("", "", "", "", this.id);
+            }, true);
+
+            heading.appendChild(titlelink);
+            heading.appendChild(document.createElement("br"));
+            heading.appendChild(morelikethislink);
+
             var summary = document.createElement('p');
             // summary.style.fontWeight = "600";
             if (value["text"].length > 300) {
@@ -97,7 +133,7 @@ function displayresults(inputtext, btitle, bcontent, bcategory) {
                 summary.innerHTML = value["text"];
             }
 
-            searchresult.appendChild(titlelink);
+            searchresult.appendChild(heading);
             // searchresult.appendChild(document.createElement("br"));
             searchresult.appendChild(summary);
             // searchresult.appendChild(document.createElement("br"));
@@ -107,7 +143,7 @@ function displayresults(inputtext, btitle, bcontent, bcategory) {
             // document.getElementById("searchresults").appendChild(document.createElement("br"));
 
         });
-        
+
         //display the facets
         var index = 0;
         var facetArray = queryresults.facet_counts.facet_fields.cat;
@@ -126,7 +162,7 @@ function displayresults(inputtext, btitle, bcontent, bcategory) {
             link.textContent = facetArray[index] + " (" + facetArray[index + 1] + ") ";
             link.id = facetArray[index];
             // link.href = "link";
-            link.addEventListener("click", function () {
+            link.addEventListener("click", function() {
                 filterQuery(this.id);
             }, true);
 
@@ -138,7 +174,7 @@ function displayresults(inputtext, btitle, bcontent, bcategory) {
         facetblock.appendChild(document.createElement("br"));
         var link = document.createElement('a');
         link.textContent = "Clear Selection";
-        link.addEventListener("click", function () {
+        link.addEventListener("click", function() {
             filterQuery("");
         }, true);
         facetblock.appendChild(link);
